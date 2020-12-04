9af15dae40f19a96528dde053ddab302b9b28372
on3dd <onedeadwave@gmail.com>
2020-12-05 00:46:51 +1000

    upd

--- src/interpreter/index.ts
+++ src/interpreter/index.ts
@@ -1,14 +1,18 @@
+type EmojifuckInterpreterProgram = string;
+
 interface EmojifuckInterpreterConstructor {
   config?: EmojifuckInterpreterConfig;
 }
 
 interface EmojifuckInterpreter
   extends EmojifuckInterpreterConstructor {
+  program: EmojifuckInterpreterProgram;
   state: EmojifuckInterpreterState;
   config: EmojifuckInterpreterConfig;
 }
 
 interface EmojifuckInterpreterState {
+  output: string;
   ipointer: number;
   mpointer: number;
   astack: number[];
@@ -33,6 +37,7 @@ class EmojifuckInterpreterImpl implements EmojifuckInterpreter {
   public program: string = '';
 
   public state: EmojifuckInterpreterState = {
+    output: '',
     ipointer: 0,
     mpointer: 0,
     astack: [],
@@ -57,8 +62,69 @@ class EmojifuckInterpreterImpl implements EmojifuckInterpreter {
     this.config = props.config ?? this.config;
   }
 
-  public reset() {
+  public interpret(program: string) {
+    this.reset(program);
+
+    while (this.program[this.state.ipointer]) {
+      switch (this.program[this.state.ipointer]) {
+        case this.config.alphabet['>']: {
+          this.handleNext();
+
+          break;
+        }
+
+        case this.config.alphabet['<']:
+          this.handlePrev();
+
+          break;
+
+        case this.config.alphabet['+']:
+          this.handleAdd();
+
+          break;
+
+        case this.config.alphabet['-']:
+          this.handleSub();
+
+          break;
+
+        case this.config.alphabet['.']:
+          this.handleSet();
+
+          break;
+
+        case this.config.alphabet[',']:
+          this.handleGet();
+
+          break;
+
+        case this.config.alphabet['[']:
+          this.handleLoopStart();
+
+          break;
+
+        case this.config.alphabet[']']:
+          this.handleLoopEnd();
+
+          break;
+
+        default:
+          this.handleDefault();
+
+          break;
+      }
+
+      this.next();
+    }
+
+    return this.state.output;
+  }
+
+  private reset(program: string) {
+    this.program = program;
+
     this.state = {
+      output: '',
       ipointer: 0,
       mpointer: 0,
       astack: new Array<number>(),
@@ -66,87 +132,70 @@ class EmojifuckInterpreterImpl implements EmojifuckInterpreter {
     };
   }
 
-  public interpret(program: string) {
-    this.reset();
+  private next() {
+    this.state.ipointer += 1;
+  }
 
-    let output = '';
-
-    while (program[this.state.ipointer]) {
-      switch (program[this.state.ipointer]) {
-        case this.config.alphabet['>']: {
-          if (this.state.mpointer == this.state.memory.length - 1) {
-            this.state.memory.push(0, 0, 0, 0, 0);
-          }
-
-          this.state.mpointer++;
-
-          break;
-        }
-
-        case this.config.alphabet['<']:
-          if (this.state.mpointer > 0) {
-            this.state.mpointer--;
-          }
-
-          break;
-
-        case this.config.alphabet['+']:
-          this.state.memory[this.state.mpointer]++;
-
-          break;
-
-        case this.config.alphabet['-']:
-          this.state.memory[this.state.mpointer]--;
-
-          break;
-
-        case this.config.alphabet['.']:
-          output += String.fromCharCode(
-            this.state.memory[this.state.mpointer],
-          );
-
-          break;
-
-        case this.config.alphabet[',']:
-          this.state.memory[this.state.mpointer] = 0;
-
-          break;
-
-        case this.config.alphabet['[']:
-          if (this.state.memory[this.state.mpointer]) {
-            // If non-zero
-            this.state.astack.push(this.state.ipointer);
-          } else {
-            // Skip to matching right bracket
-            let count = 0;
-
-            while (true) {
-              this.state.ipointer++;
-              if (!program[this.state.ipointer]) break;
-
-              if (program[this.state.ipointer] === '[') count++;
-              else if (program[this.state.ipointer] === ']') {
-                if (count) count--;
-                else break;
-              }
-            }
-          }
-          break;
-
-        case this.config.alphabet[']']:
-          //Pointer is automatically incremented every iteration, therefore we must decrement to get the correct value
-          this.state.ipointer = this.state.astack.pop() - 1;
-          break;
-
-        default:
-          // We ignore any character that are not part of regular Brainfuck syntax
-          break;
-      }
-
-      this.state.ipointer += 1;
+  private handleNext() {
+    if (this.state.mpointer == this.state.memory.length - 1) {
+      this.state.memory.push(0, 0, 0, 0, 0);
     }
 
-    return output;
+    this.state.mpointer++;
+  }
+
+  private handlePrev() {
+    if (this.state.mpointer > 0) {
+      this.state.mpointer--;
+    }
+  }
+
+  private handleAdd() {
+    this.state.memory[this.state.mpointer]++;
+  }
+
+  private handleSub() {
+    this.state.memory[this.state.mpointer]--;
+  }
+
+  private handleSet() {
+    this.state.output += String.fromCharCode(
+      this.state.memory[this.state.mpointer],
+    );
+  }
+
+  private handleGet() {
+    this.state.memory[this.state.mpointer] = 0;
+  }
+
+  private handleLoopStart() {
+    if (this.state.memory[this.state.mpointer]) {
+      // If non-zero
+      this.state.astack.push(this.state.ipointer);
+    } else {
+      // Skip to matching right bracket
+      let count = 0;
+
+      while (true) {
+        this.state.ipointer++;
+        if (!this.program[this.state.ipointer]) break;
+
+        if (this.program[this.state.ipointer] === '[') count++;
+        else if (this.program[this.state.ipointer] === ']') {
+          if (count) count--;
+          else break;
+        }
+      }
+    }
+  }
+
+  private handleLoopEnd() {
+    //Pointer is automatically incremented every iteration, therefore we must decrement to get the correct value
+    this.state.ipointer = this.state.astack.pop() - 1;
+  }
+
+  private handleDefault() {
+    // We ignore any character that are not part of regular Brainfuck syntax
   }
 }
 
